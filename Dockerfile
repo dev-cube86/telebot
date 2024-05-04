5527d49106e24b31231a8ee75529eec80e40d711
dev-cube86 <164399084+dev-cube86@users.noreply.github.com>
2024-05-04 12:56:06 +0300

    Develop (#1)
    * added develop branch
    
    * changed go version
    
    * added docker login
    
    * added build and push step
    
    * fixed errors
    
    * fixed errors 2
    
    * fixed errors 3
    
    * test
    
    * test 2
    
    * changed helm variables
    
    * added git steps
    
    * added indentation
    
    * commented unused workflow code
    
    * reverted last changes
    
    * updated version v.1.0.1-1b09b7c
    
    * changed registry and repository for container image
    
    * updated version v.1.0.1-71452a8
    
    * changed registry in workflow yaml
    
    * changed registry name
    
    * fixes
    
    * set auth for github packages
    
    * use github actions auth
    
    * added permissions
    
    * changed app name
    
    * changed registry
    
    * updated version v.1.0.1-1e35a5c
    
    * changed repository in helm chart
    
    * updated version v.1.0.1-a3a86aa
    
    * renamed helm directory
    
    * changed helm dir
    
    * updated version v.1.0.1-bb11167
    
    * added Jenkins file
    
    * updated version v.1.0.1-2ec6ba0
    
    * fixed error
    
    * updated version v.1.0.1-0ea685e
    
    * uncommented version in makefile
    
    * updated jenkinsfile stage
    
    * updated version v.1.0.1-2890d84
    
    * changed makefile variables
    
    * push tags
    
    * added VERSION to jenkinsfile
    
    * fixed makefile
    
    * fixed makefile
    
    * updated version v.1.0.1-97bbea5
    
    * changed dockerfile
    
    * updated version v.1.0.1-c9110bf
    
    ---------
    
    Co-authored-by: Dmytro_Dehtiarov1 <Dmytro_Dehtiarov1@epam.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM quay.io/projectquay/golang:1.22 as builder
-ARG TARGETOS
+ARG OS
 ARG GOARCH
 WORKDIR /go/src/app
 COPY . .
-RUN make $TARGETOS
+RUN make $OS
 
 FROM scratch
 WORKDIR /
 COPY --from=builder /go/src/app/telebot .
 COPY --from=alpine:latest /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+# LABEL org.opencontainers.image.source=https://https://github.com/dev-cube86/telebot
 ENTRYPOINT ["./telebot"]
---
+++ jenkins.groovy
@@ -0,0 +1,56 @@
+pipeline {
+    agent any
+
+    parameters {
+        choice(name: 'OS', choices: ['linux', 'windows', 'ios'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', '386', 'arm64', 'arm'], description: 'Pick ARCH')
+    }
+
+    environment {
+        REPO = 'https://github.com/dev-cube86/telebot'
+        BRANCH = 'develop'
+    }
+
+    stages {
+
+        stage('clone') {
+            steps {
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage('test') {
+            steps {
+                echo 'Testing started'
+                sh "make test"
+            }
+        }
+
+        stage('build') {
+            steps {
+                echo "Building binary started"
+                sh "make build"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Building image started"
+                sh "make image"
+            }
+        }
+
+        stage('login to GHCR') {
+            steps {
+                sh "echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_TOKEN_USR --password-stdin"
+            }
+        }
+        
+        stage('push image') {
+            steps {
+              sh "make push"
+            }
+        } 
+    }
+}    
